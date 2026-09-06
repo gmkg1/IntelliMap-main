8776b8d3c6c22601ef369ff87ce4eee22c603b46
gmkg1 <maheengmk@gmail.com>
2026-09-07 00:26:16 +0530

    final dep

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-# Ensure the root project directory is on sys.path
-root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if root_dir not in sys.path:
-    sys.path.insert(0, root_dir)
-
-from flask_app import app
